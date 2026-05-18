f7e36b23771b49f60e3249025f0261119287f095
Amir-0605 <aashirbaev89@gmail.com>
2026-05-18 21:10:42 +0500

    update

--- src/companents/Header/Header.jsx
+++ src/companents/Header/Header.jsx
@@ -8,7 +8,7 @@ import img1 from "../assets/img1.webp"
 import img2 from "../assets/img2.webp"
 import img3 from "../assets/img3.webp"
 import img11 from "../assets/img11.webp"
-import logo from "../assets/logo.png"
+import logo from "../assets/bgimg (1).webp"
 import { FaPhoneAlt } from "react-icons/fa";
 
 export default function MobileFirstLayout() {
@@ -39,13 +39,13 @@ export default function MobileFirstLayout() {
         <div className="absolute inset-0 h-380 pt-190 w-full bg-transparent text-white overflow-hidden lg:px-10 lg:mx-auto">
 
             {/* HEADER */}
-            <header className="fixed top-0 left-0 right-0 z-40 border-b border-white/10 backdrop-blur-md bg-[#29494C] shadow-[0_6px_30px_#00000035]">
+            <header className="fixed top-0 left-0 right-0 z-40 border-b border-white/10 backdrop-blur-md bg-[#ffffff22] shadow-[0_6px_30px_#00000035]">
                 <div className="mx-auto flex h-[70px] max-w-[1400px] items-center justify-between px-4 md:px-6 lg:px-10">
 
                     <img
                         src={logo}
                         alt="logo"
-                        className="h-10 sm:h-12 md:h-14 lg:h-16 w-30 sm:w-32 md:w-36 lg:w-40 object-cover opacity-100 mix-blend-screen"
+                        className="h-10 sm:h-12 md:h-14 lg:h-16 w-30 sm:w-32 md:w-36 lg:w-40 object-cover opacity-100 text-[#F0F8FF]"
                     />
 
                     <div className="flex gap-3 sm:gap-5 md:gap-7 items-center">
